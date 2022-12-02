f7ed3c7b21818ebdfdd325f60fcc59935b4bb7f1
Alex Shank <ashank31@gmail.com>
2022-12-02 11:26:18 -0500

    Day 2 solution.

--- day-1.py
+++ day-1.py
@@ -1,12 +1,7 @@
-from collections import defaultdict
-
-def load_data():
-	with open("day-1-input.txt", "r") as input_file:
-		data = input_file.read().split('\n')
-	return data
+from helpers import load_data
 
 if __name__ == "__main__":
-	data = load_data()
+	data = load_data("day-1-input.txt")
 	# only work with ints
 	data = [int(x) if x != '' else 0 for x in data]
 	# find indices with zeros
@@ -16,7 +11,3 @@ if __name__ == "__main__":
 	# print sum of top 3
 	sums.sort()
 	print(sum(sums[-3:]))
-
-	d = defaultdict(lambda: 0)
-	d['cat'] += 5
-	print(d['cat'])
---
+++ day-2.py
@@ -0,0 +1,27 @@
+from helpers import load_data
+
+if __name__ == "__main__":
+	data = load_data("day-2-input.txt")
+
+	# part 1
+	total = 0
+	for d in data:
+		opponent = ord(d[0]) - 65 	# map A, B, C to 0, 1, 2
+		me = ord(d[2]) - 88					# map X, Y, Z to 0, 1, 2
+		total += me + 1							# points from the chosen rock, paper, or scissors
+		match opponent - me:
+			case 0: total += 3
+			case -1 | 2: total += 6
+			case -2 | 1: total += 0		# explicit
+	print(total)
+
+	# part 2
+	total = 0
+	for d in data:
+		opponent = ord(d[0]) - 65 	# map A, B, C to 0, 1, 2
+		# choose rock, paper, scissors based on if we should lose, tie, or win
+		match d[2]:
+			case 'X': total += 0 + (opponent - 1) % 3 + 1
+			case 'Y': total += 3 + opponent + 1
+			case 'Z': total += 6 + (opponent + 1) % 3 + 1
+	print(total)
