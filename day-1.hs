d6574e945173daf34421a17eb7f7a5cd0bfa8b7a
Alex Shank <ashank31@gmail.com>
2022-12-02 23:55:53 -0500

    My first Haskell program :)

--- day-1.hs
+++ day-1.hs
@@ -1,19 +1,36 @@
 import System.IO
+import Data.List
 
 main = do
-  handle <- openFile "day-1-input.txt" ReadMode
-  contents <- hGetContents handle
-  putStr contents
-  hClose handle
-  return contents
+  inputData <- readFile "day-1-input.txt"
+  let inputLines = lines inputData
 
+  -- create list of tuples containing sublist ranges (add boundary indices)
+  let enumeratedLines = zip [0..] inputLines
+  let blankIndices = [-1] ++ [fst x | x <- enumeratedLines, null (snd x)] ++ [length inputLines]
+  let adjIndices = zipAdj blankIndices
 
-// once I understand Haskell better, I can use this (probably bad) way of getting the input text
-// then we can mess around in the interactive REPL
-ghci> :l day-1.hs
-ghci> temp :: String <- main
-ghci> temp
-ghci> lines contents // at this point, have a list of strings and are ready to solve!
+  -- create list of sublists, then reduce each sublist to its sum
+  let subLists = [slice (fst x + 1) (snd x) inputLines | x <- adjIndices]
+  let sums = [sum (map strToInt xs) | xs <- subLists]
 
+  -- part 1
+  print $ maximum sums
 
+  -- part 2 (sum the last 3 elements of sorted array)
+  let sumCount = length sums
+  print $ sum $ slice (sumCount - 3) sumCount $ sort sums
+  -- ($) :: (a -> b) -> a -> b
+  -- the above is same as: print (sum (slice (sumCount - 3) sumCount (sort sums)))
+  
+-- take [1,2,3] and create [(1,2), (2,3)]
+zipAdj :: [a] -> [(a, a)]
+zipAdj xs = zip xs (tail xs)
 
+-- basically python's slicing functionality xs[from:to]
+slice :: Int -> Int -> [a] -> [a]
+slice from to xs = take (to - from) (drop from xs)
+
+-- we need to convert to Int (sum undefined for String)
+strToInt :: String -> Int
+strToInt str = read str::Int
