3afa590a170166f095fa8b1a7f24dbe48c430059
Alex Shank <ashank31@gmail.com>
2022-12-20 19:45:01 -0600

    Parsing day 16 input.

--- day-16.py
+++ day-16.py
@@ -2,4 +2,21 @@ from helpers import load_data
 
 if __name__ == "__main__":
 	data = load_data("day-16-input.txt")
-	print(data)
+
+	valve_flow_rates = {}
+	valve_connections = {}
+	for d in data:
+		valve = d[6:8]
+		d = d[23:]
+		d = d.replace('valves', 'valve').replace('tunnels', 'tunnel').replace('leads', 'lead')
+		first, second = d.split('; tunnel lead to valve ')
+
+		valve_flow_rates[valve] = int(first)
+		valve_connections[valve] = second.split(', ')
+	
+	print(valve_flow_rates)
+	print(valve_connections)
+	starting_valve = 'AA'
+
+
+
