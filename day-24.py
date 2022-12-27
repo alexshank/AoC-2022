a290baecbc2b4fc2732e3c60aa9b9df5b611a422
Alex Shank <ashank31@gmail.com>
2022-12-27 00:17:37 -0500

    Still running too slowly.

--- day-24.py
+++ day-24.py
@@ -1,11 +1,9 @@
 from helpers import load_data
 
 class State:
-	def __init__(self, blizzards, traveler, destination, state_depth):
+	def __init__(self, blizzards, traveler, state_depth):
 		self.blizzards = blizzards
 		self.traveler = traveler
-		# TODO always the same
-		self.destination = destination
 		self.state_depth = state_depth
 
 def print_board(squares, x_count, y_count, traveler):
@@ -24,24 +22,10 @@ def print_board(squares, x_count, y_count, traveler):
 					print(len(squares[complex(x, y)]), end='')
 		print()
 
-	# TODO do we need better "not seen" logic?
-# def not_already_seen(test_state, seen_states):
-# 	for seen_state in seen_states:
-# 		if test_state.traveler == seen_states.traveler:
-# 			if test_state.blizzards in seen_state.blizzard:
-# 	return True
-
+# TODO could speed up by updating blizzards in place?
 def update_blizzard_locations(squares, board_width, board_height):
 	result = {}
 	for coord, symbols in squares.items():
-
-		# TODO unsure if needed
-		if len(symbols) == 0:
-			# TODO it is... with the empty spaces
-			# print('Is this happening?')
-			# print(coord)
-			continue
-
 		for symbol in symbols:
 			match symbol:
 				case '<':
@@ -61,47 +45,63 @@ def update_blizzard_locations(squares, board_width, board_height):
 
 	return result
 
+# TODO this is likely slow
 def possible_traveler_squares(updated_squares, traveler, destination, board_width, board_height):
-	potential_squares = [traveler, traveler + complex(-1, 0), traveler + complex(1, 0), traveler + complex(0, -1),traveler + complex(0, 1)]
-
 	result = []
+	potential_squares = [traveler, traveler + complex(-1, 0), traveler + complex(1, 0), traveler + complex(0, -1),traveler + complex(0, 1)]
 	for potential_square in potential_squares:
 		# TODO the potential_square == traveler just allows the elf to stay in the start position
 		if (potential_square == destination) or (potential_square == traveler) or (potential_square.real >= 0 and potential_square.real < board_width and potential_square.imag >= 0 and potential_square.imag < board_height):
-			# TODO I think this check creates extra entries in our map
 			if potential_square not in updated_squares.keys():
 				result.append(potential_square)
 	return result
 
 # TODO how can we distinguish states properly for "seen" flag?
 def bfs(blizzards, traveler, destination, board_width, board_height):
-	first_state = State(blizzards, traveler, destination, 0)
+	first_state = State(blizzards, traveler, 0)
 	queue = [first_state]
 	seen_states = set() # TODO not sure this will work correctly
-
+	
+	# TODO remove
+	traveler_set = set()
+	previous_size = 0
 	while len(queue) > 0:
 		current_state = queue.pop(0)
 
-		if current_state.state_depth % 10 == 0:
-			print(current_state.state_depth)
-		
-		if current_state.traveler == current_state.destination:
+		# TODO we're considering way too many paths. unique traveler locations are increasing, but slowly
+		traveler_set.add(current_state.traveler)
+		if len(traveler_set) > previous_size:
+			previous_size = len(traveler_set)
+			print('Unique traveler locations seen: ' + str(len(traveler_set)))
+		# print(str(len(seen_states)))
+
+		# TODO not even getting to depth 20 :O	
+		if current_state.traveler == destination:
 			return current_state.state_depth 
 
 		new_blizzards = update_blizzard_locations(current_state.blizzards, board_width, board_height)
 
-		potential_moves = possible_traveler_squares(new_blizzards, current_state.traveler, current_state.destination, board_width, board_height)
+		potential_moves = possible_traveler_squares(new_blizzards, current_state.traveler, destination, board_width, board_height)
 		for potential_move in potential_moves:
-			new_state = State(new_blizzards, potential_move, destination, current_state.state_depth + 1)
-			if new_state not in seen_states:
+			new_state = State(new_blizzards, potential_move, current_state.state_depth + 1)
+
+			# TODO this has to be crazy slow
+			already_seen = False
+			for state in seen_states:
+				if are_states_equal(new_state, state):
+					# print('already saw')
+					already_seen == True
+					break
+
+			if not already_seen:
 				seen_states.add(new_state)
 				queue.append(new_state)
-			else:
-				print('already saw')
+
+def are_states_equal(state_1, state_2):
+	return state_1.traveler == state_2.traveler and state_1.blizzards == state_2.blizzards
 
 if __name__ == "__main__":
-	data = load_data("day-24-test-input.txt")
-	print()
+	data = load_data("day-24-input.txt")
 
 	# load data into a data structure
 	row_count = len(data)
@@ -129,6 +129,18 @@ if __name__ == "__main__":
 	y_count = row_count - 2
 
 	print(bfs(squares, traveler, destination, x_count, y_count))
+
+	# test_dict_1 = {1: '1', 2: '2'}
+	# test_dict_2 = {2: '2', 1: '1'}
+	# traveler_1 = complex(2,2)
+	# traveler_2 = complex(2,2)
+	# destination_1 = complex(3,3)
+	# destination_2 = complex(3,3)
+
+	# test_state_1 = State(test_dict_1, traveler_1, destination_1, 5)
+	# test_state_2 = State(test_dict_2, traveler_2, destination_2, 5)
+	# print(test_state_1 == test_state_2)
+	# print(are_states_equal(test_state_1, test_state_2))
 
 
 
