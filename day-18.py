b9c1df87e1b35207650762a95da7092844b0c26a
Alex Shank <ashank31@gmail.com>
2022-12-27 03:36:44 -0500

    Completed day 18.

--- day-18.py
+++ day-18.py
@@ -1,50 +1,47 @@
 from helpers import load_data
 
+def adjacent_cubes(x, y, z):
+	return [(x - 1, y, z), (x + 1, y, z), (x, y - 1, z), (x, y + 1, z), (x, y, z - 1), (x, y, z + 1)]
+
+def count_touching_edges(set_a, set_b):
+	result = 0
+	for point_a in set_a:
+		for test_point in adjacent_cubes(*point_a):
+			if test_point in set_b:
+				result += 1
+	return result
+
+# bfs "expands" the outside air around the lava blob
+def bfs(full_space, lava_drops):
+	outside_air = set()
+	queue = [(-5, -5, -5)] # space we know is outside of lava
+	while len(queue) > 0:
+		for point in adjacent_cubes(*queue.pop(0)):
+			if point not in full_space:
+				continue
+
+			if point not in lava_drops and point not in outside_air:
+				outside_air.add(point)
+				queue.append(point)
+	return outside_air
+	
 if __name__ == "__main__":
-	# parse data
 	data = load_data("day-18-input.txt")
-	lava_drops = set()
-	for d in data:
-		x, y, z = d.split(',')
-		lava_drops.add((int(x), int(y), int(z)))
+	lava_drops = {tuple(map(int, d.split(','))) for d in data}
+
+	# comprehensive set is inefficient, but allows us to ignore edge conditions
+	LOWER_LIMIT, UPPER_LIMIT = -10, 30
+	full_space = set()
+	for x in range(LOWER_LIMIT, UPPER_LIMIT):
+		for y in range(LOWER_LIMIT, UPPER_LIMIT):
+			for z in range(LOWER_LIMIT, UPPER_LIMIT):
+				full_space.add((x, y, z))
+	non_lava = full_space - lava_drops
 
 	# part 1 (answer: 4364)
-	untouched_sides = 0
-	for x, y, z in lava_drops:
-		if (x + 1, y, z) not in lava_drops:
-			untouched_sides += 1
-		if (x - 1, y, z) not in lava_drops:
-			untouched_sides += 1
-		if (x, y + 1, z) not in lava_drops:
-			untouched_sides += 1
-		if (x, y - 1, z) not in lava_drops:
-			untouched_sides += 1
-		if (x, y, z + 1) not in lava_drops:
-			untouched_sides += 1
-		if (x, y, z - 1) not in lava_drops:
-			untouched_sides += 1
-	print(untouched_sides)
+	print(count_touching_edges(lava_drops, non_lava))
 
-	# part 2 (answer: )
-	x_y_coverage = {}
-
-	for x in range(22):
-		for y in range(22):
-			x_y_coverage[(x, y)] = min
-
-
-	y_z_coverage = set()
-	x_z_coverage = set()
-	for z in range(22):
-		x_y_coverage.add((x, y))
-		y_z_coverage.add((y, z))
-		x_z_coverage.add((x, z))
-	print(2 * len(x_y_coverage) + 2 * len(y_z_coverage) + 2 * len(x_z_coverage))
-
-
-
-
-
-
-
+	# part 2 (answer: 2508)
+	outside_air = bfs(full_space, lava_drops)
+	print(count_touching_edges(outside_air, lava_drops))
 
