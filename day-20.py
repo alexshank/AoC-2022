db5d38824351ecf8a12a367cedbe0e3b63cf5b85
Alex Shank <ashank31@gmail.com>
2023-01-05 12:57:31 -0600

    Simplifying logic. Still not getting challenge input correct?

--- day-20.py
+++ day-20.py
@@ -1,8 +1,8 @@
 from helpers import load_data
 
 
-# TODO next potential issue: when we wrap multiple times, we probably need to increment many times
-
+# TODO need to create a larger example case with zero in it and numbers that are > 3 * len of list
+# TODO next potential issue: when we wrap multiple times, we probably need to increment many times??
 
 def find_current_index_by_original_index(target_index):
 	for k, v in mix_numbers.items():
@@ -14,13 +14,21 @@ class MixNumber:
 		self.original_index = original_index
 		self.value = value
 
+def swap(mix_numbers, left_index, right_index):
+		temp = mix_numbers[left_index]
+		mix_numbers[left_index] = mix_numbers[right_index]
+		mix_numbers[right_index] = temp
+
 if __name__ == "__main__":
-	data = load_data("day-20-test-input.txt")
+	data = load_data("day-20-input.txt")
 	data = [int(d) for d in data]
 
 	# key is the current index of the value
 	mix_numbers = {i: MixNumber(i, val) for i, val in enumerate(data)}
 	mix_len = len(mix_numbers)
+
+	# print([v.value for v in mix_numbers.values()])
+	# print()
 
 	for i in range(mix_len):
 		# print(str(i) + ' of ' + str(mix_len))
@@ -30,44 +38,45 @@ if __name__ == "__main__":
 		start_index = find_current_index_by_original_index(i)
 		movement = mix_numbers[start_index].value
 
-		if movement == 0:
-			print('no movement')
-			continue
+		print(f'Starting at: {start_index}')
+		print(f'Moving: {movement}')
 
-		end_index = (start_index + movement) % mix_len
-
-		# TODO problem just has a strange way of moving numbers around???
-		if movement > 0 and (end_index == mix_len - 1 or end_index < start_index):
-			# wrap_count = abs((start_index + movement) // mix_len)	# TODO confirm this on paper first
-			end_index = (end_index + 1) % mix_len
-		elif movement < 0 and (end_index == 0 or end_index > start_index):
-			# wrap_count = abs((start_index + movement) // mix_len) # TODO confirm this on paper first
-			end_index = (end_index - 1) % mix_len
-
-		# print('moving ' + str(mix_numbers[start_index].value) + ' between ' + str(mix_numbers[(end_index) % mix_len].value) + ' and ' + str(mix_numbers[(end_index + 1) % mix_len].value))
-
-		# save what we'll overwrite
-		moving_spot = mix_numbers[start_index]
-
-		if start_index > end_index:
-
-			# print('heerree')
-			# print(start_index)
-			# print(end_index)
-			for i in reversed(range(end_index, start_index)):
-				# print((i+1, i))
-				mix_numbers[i + 1] = mix_numbers[i]
-			# print(end_index)
-			mix_numbers[end_index] = moving_spot
-
-		elif start_index < end_index:
-
-			for i in range(start_index, end_index):
-				mix_numbers[i] = mix_numbers[i + 1]
-			mix_numbers[end_index] = moving_spot
+		# moving left
+		if movement < 0:
+			left = abs(movement) % mix_len
+			if left == 0:
+				print('skipping left')
+				print()
+				continue
+			elif start_index - left <= 0: # TODO should this be zero inclusive? really?
+				right = mix_len - 1 - left
+				print(f'Right: {right}')
+				for i in range(right):
+					swap(mix_numbers, start_index + i, start_index + 1 + i)
+			else:
+				print(f'Left: {left}')
+				for i in range(left):
+					swap(mix_numbers, start_index - 1 - i, start_index - i)
+		# moving right
+		else:
+			right = movement % mix_len
+			if right == 0:
+				print('skipping right')
+				print()
+				continue
+			elif start_index + right >= mix_len:
+				left = mix_len - 1 - right
+				print(f'Left: {left}')
+				for i in range(left):
+					swap(mix_numbers, start_index - 1 - i, start_index - i)
+			else:
+				print(f'Right: {right}')
+				for i in range(right):
+					swap(mix_numbers, start_index + i, start_index + 1 + i)
+		print()
 
 		# print([v.value for v in mix_numbers.values()])
-		# print()
+		print()
 
 print('-----------------')
 print([v.value for v in mix_numbers.values()])
@@ -82,6 +91,7 @@ for k, v in mix_numbers.items():
 		break
 print(index_of_zero)
 
+# TODO guessed 2487, was too low
 print('summing')
 print(mix_numbers[(index_of_zero + 1_000) % mix_len].value)
 print(mix_numbers[(index_of_zero + 2_000) % mix_len].value)
