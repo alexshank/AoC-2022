fa7c3e546838f3cc26f95b7a9e9708ca58082085
Alex Shank <ashank31@gmail.com>
2023-01-08 21:39:45 -0600

    Completed day 25 part 1.

--- day-25.py
+++ day-25.py
@@ -6,40 +6,65 @@ def char_to_num(c):
 		case '=': return -2
 		case _: return int(c)
 
-def increment_snafu(old_snafu):
-	match old_snafu[-1]:
-		case '2':
-			if len(old_snafu) == 1:
-				return "1="
-			else:
-				return increment_snafu(old_snafu[:-1]) + "="
-		case '=':
-			return old_snafu[:-1] + "-"
-		case '-':
-			return old_snafu[:-1] + "0"
-		case '0':
-			return old_snafu[:-1] + "1"
-		case '1':
-			return old_snafu[:-1] + "2"
+def decrement_char(old_char):
+	match old_char:
+		case '2': return '1'
+		case '1': return '0'
+		case '0': return '-'
+		case '-': return '='
+		case _: raise Exception('Cannot decrement')
+
+def decrement_at_index(str_number, index):
+	old_char = str_number[index]
+	new_char = decrement_char(old_char)
+	result = str_number[:index] + new_char + str_number[index + 1:]
+	return result
+
+def weird_num_to_decimal(str_num):
+	result = 0
+	for i, c in enumerate(reversed(str_num)):
+		val = char_to_num(c)
+		result += val * (5 ** i)
+	return result
+	
 
 if __name__ == "__main__":
 	data = load_data("day-25-input.txt")
 
-	total = 0
-	for d in data:
-		sub_sum = 0
-		for i, c in enumerate(reversed(d)):
-			val = char_to_num(c)
-			sub_sum += val * (5 ** i)
-		total += sub_sum
+	total = sum([weird_num_to_decimal(d) for d in data])
+	# total = 36067808580527
 	print(total)
 
-	# temp = "0"
-	# for i in range(total):
-	# 	temp = increment_snafu(temp)
-	# 	if i % 10_000 == 0:
-	# 		print(temp)
-	# print(temp)
+	powers_2 =[2 * 5 ** i for i in range(30)] 
+	for i in range(len(powers_2)):
+		if total < sum(powers_2[:i]):
+			num_digits = i
+			print(f'{num_digits} digits')
+			break
+
+	# part 1 (answer: 2-212-2---=00-1--102)
+	answer = '2' * num_digits
+	found = False
+	for i in range(num_digits):
+		# at most 2 -> =, or 4 decrements
+		candidate = answer
+		for _ in range(4):
+			candidate = decrement_at_index(candidate, i)
+			test_val = weird_num_to_decimal(candidate)
+			if test_val == total:
+				found = True
+				answer = candidate
+				break
+			elif test_val > total:
+				answer = candidate
+			else:
+				break
+		
+		if found == True:
+			break
+
+	print(answer)
+	
 
 
 
